be22a338728ba566634bb8ac9fdf88dc10ee9550
mgheeraert <43027758+mgheeraert@users.noreply.github.com>
2018-10-11 10:00:36 +0200

    crocodil

---
+++ 06-condities/crocodile paradox.py
@@ -0,0 +1,9 @@
+gebruiker = (input('wie is de gebruiker?'))
+
+#invoer
+if gebruiker == 'vader':
+    print('Krokodil geeft kind terug')
+elif gebruiker == 'krokodil':
+    print('Krokodil eet kind op')
+else:
+    print('Moe van het denken')
